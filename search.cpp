06646775aafa5ae9def0bb48b563ed2060b3d1b5
Lavínia Silva Dias <l4viniadias510@gmail.com>
2024-03-06 20:18:08 -0300

    Update function to separate into blocks

--- search.cpp
+++ search.cpp
@@ -6,19 +6,52 @@
 using namespace std;
 
 
-void read_text(string filename){
-    fstream books;
-    books.open(filename,ios::in); //open a file to perform read operation using file object
-    if (books.is_open()){ //checking whether the file is open
-      string content;
-      while(getline(books, content)){ //read data from file object and put it into string.
-         cout << content << "\n"; //print the data of the string
-      
-      break;}
-      books.close(); //close the file object.
-    }   
+// Function to read the text from a file and divide it into blocks for each thread
+vector<string> read_text(string filename, int iNumThreads){
+    // Open the file
+    ifstream file;
+    file.open(filename);
+    string line;
+    // Calculate the number of lines in the file 
+    int num_lines = 0;
+    while(getline(file, line)){
+        num_lines++;
+    }
+
+    // Create the blocks of text for each thread based on the number of lines in the file
+    vector<string> parts;
+    int block_size = num_lines / iNumThreads;
+    int start = 0;
+    int end = block_size;
+    int remainder = num_lines % iNumThreads;
+
+    for(int i = 0; i < iNumThreads; i++){
+        string part = "";
+        if(i == iNumThreads - 1){
+            end += remainder;
+        }
+        file.clear();
+        file.seekg(0, ios::beg);
+        for(int j = 0; j < num_lines; j++){
+            getline(file, line);
+            if(j >= start && j < end){
+                part += line + "\n";
+            }
+        }
+        parts.push_back(part);
+        start = end;
+        end += block_size;
+    }
+    return parts;
 }
 
+
+void print_vector(vector<string> v){
+    for(int i = 0; i < v.size(); i++){
+        cout<< "Thread " << i << endl;
+        cout << v[i] << endl;
+    }
+}
 
 
 
@@ -28,15 +61,19 @@ int main(){
 
 
     string document = "shakespeare.txt";
+    string document = "testando.txt";
+
     time_t start, end;
     time(&start);
-    read_text(document);
+    print_vector(read_text(document, 4));
     time(&end); 
 
     // Calculating total time taken by the program. 
     double time_taken = double(end - start); 
     cout << "Time taken by program is : " << fixed << time_taken << setprecision(5); 
     cout << " sec " << endl;
+
+    
 
 
     
