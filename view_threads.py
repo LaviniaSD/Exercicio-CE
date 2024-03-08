13d8b8fb55707c785c5c31a4f2c3f743079f0406
IsabelaYabe <103371464+IsabelaYabe@users.noreply.github.com>
2024-03-08 15:01:05 -0300

    corrigir 2 kk

--- view_threads.py
+++ view_threads.py
@@ -25,18 +25,19 @@ def view_threads_searches(csv_file):
     plt.title("Análise de Paralelismo com Threads")
     plt.show()
 
-def view_threads(csv_file):
-    df = pd.read_csv(csv_file)
+def view_threads(csv_file_1, csv_file_2):
+    df_1 = pd.read_csv(csv_file_1)
+    df_2 = pd.read_csv(csv_file_2)
 
     fig, axs = plt.subplots(1, 2, figsize=(12, 6)) 
 
-    axs[0].scatter(df["A"], df["B"]) 
+    axs[0].scatter(df_1["A"], df_1["B"]) 
 
     axs[0].set_xlabel("Número de Threads") 
     axs[0].set_ylabel("Tempo de Execusão")  
     axs[0].set_title("Análise de Paralelismo com Threads")  
 
-    axs[1].scatter(df["A"], df["B"]) 
+    axs[1].scatter(df_2["A"], df_2["B"]) 
 
     axs[1].set_xlabel("Número de Threads")  
     axs[1].set_ylabel("Tempo de Execusão da Pesquisa")  
