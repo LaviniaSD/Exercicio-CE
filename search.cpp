dcd6fc4e9e254e630fb8e3ae350132764e111a99
Lavínia Silva Dias <l4viniadias510@gmail.com>
2024-03-08 07:11:00 -0300

    Create the return struct

--- search.cpp
+++ search.cpp
@@ -1,15 +1,16 @@
+#include <fstream>
 #include <iostream>
 #include <iomanip>
 #include <regex>
-#include <fstream>
 #include <string>
-#include <vector>
 #include <thread>
+#include <vector>
+#include "search.h"
 using namespace std;
 
 
 // Function to count the number of occurrences of two words in a string ignoring punctuation
-vector<int> count_words(int threadID,vector<vector<int>> count_vector ,string text, string word1, string word2){
+void count_words(int threadID,vector<vector<int>> count_vector ,string text, string word1, string word2){
     // Remove punctuation from the text
     text = regex_replace(text, regex("[^a-zA-Z\\s]"), "");
     // Convert the text to lowercase
@@ -27,9 +28,21 @@ vector<int> count_words(int threadID,vector<vector<int>> count_vector ,string te
             count2++;
         }
     }
-    return {count1, count2};
+    
+    // Update the vector with the count of words for the thread
+    count_vector[threadID-1] = {count1, count2};
 }
 
+// Function to sum the counts of words from each thread
+vector<int> somaThread(vector<vector<int>> vec, int numThreads){
+    int sum1 = 0;
+    int sum2 = 0;
+    for(int i = 0; i < numThreads; i++){
+        sum1 += vec[i][0];
+        sum2 += vec[i][1];
+    }
+    return {sum1, sum2};
+}
 
 // Function to read the text from a file and divide it into blocks for each thread
 vector<string> read_text(string filename, int iNumThreads){
@@ -71,7 +84,21 @@ vector<string> read_text(string filename, int iNumThreads){
 }
 
 
-void process_threads(int iNumThreads, vector<string> text, string word1, string word2){
+search_return process_threads(int iNumThreads, string filename, string word1, string word2){
+
+    auto start = chrono::high_resolution_clock::now();
+
+    // Create a struct to store the return values
+    struct search_return search_return;
+    search_return.iNumThreads = iNumThreads;
+
+    // Text processing
+    auto start_text_processing = chrono::high_resolution_clock::now();
+    vector<string> parts = read_text(filename, iNumThreads);
+    auto end_text_processing = chrono::high_resolution_clock::now();
+
+    // Update the struct with the size of the text blocks
+    search_return.threadSize = parts[0].size();
 
     // Create and launch multiple threads
     std::vector<std::thread> threads;
@@ -80,23 +107,47 @@ void process_threads(int iNumThreads, vector<string> text, string word1, string 
     vector<vector<int>> count_vector;
 
     // Start measuring time
-    auto start = chrono::high_resolution_clock::now();
+    auto start_search = chrono::high_resolution_clock::now();
 
-    for(int i = 0; i < iNumThreads; ++i){
-        threads.push_back(std::thread(count_words, i,count_vector, text[i], word1, word2));
+    for(int i = 1; i <= iNumThreads; ++i){
+        count_vector.push_back({0, 0});
+        threads.push_back(std::thread(count_words, i, count_vector, parts[i-1], word1, word2));
     }
+
+    // End measuring time
 
     for(auto& thread : threads){
         thread.join();
     }
 
+    auto end_search = chrono::high_resolution_clock::now();
+
+
+    // Calculate the number of occurrences of the words
+    vector<int> vectorSoma = somaThread(count_vector, iNumThreads);
+
+
+    // Store the return values in the struct
+    search_return.loveCount = vectorSoma[0];
+    search_return.hateCount = vectorSoma[1];
+
+    // Set the most used word
+    if(vectorSoma[0] > vectorSoma[1]){
+        search_return.mostUsedWord = word1;
+    }
+    else{
+        search_return.mostUsedWord = word2;
+    }
+
     // End measuring time
     auto end = chrono::high_resolution_clock::now();
 
-    // Calculating total time taken by the program.
-    double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
-    time_taken *= 1e-9; // convert to seconds
-    cout << "Time taken by program is : " << fixed << time_taken << setprecision(9);
+    // Calculating time taken by the program for different tasks and storing it in the struct
+    search_return.processTime = chrono::duration_cast<chrono::milliseconds>(end_text_processing - start_text_processing).count();
+    search_return.searchTime = chrono::duration_cast<chrono::milliseconds>(end_search - start_search).count();
+    search_return.totalTime = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+
+    return search_return;
 
 }
 
@@ -106,12 +157,30 @@ int main(){
 
     // Text to be processed by each thread
     string document = "shakespeare.txt";
-    vector<string> parts;
+
+    // Create a struct to store the return values
+    struct search_return search_return;
+
+    // Create a vector to store the time taken by the program for different number of threads
+    vector<vector<int>> time_vector;
+
+    // Process the program for different number of threads
     for (int i = 1; i <= 100; i++){
-        // Process the text and divide it into blocks for each thread
-        parts = read_text(document, i);
-        process_threads(i, read_text(document, i), "love", "hate");
+        cout << "Number of threads: " << i << endl;
+        search_return = process_threads(i, document, "love", "hate");
+        time_vector.push_back({i, search_return.totalTime});
+        cout<<endl;
     }
+
+    // Save the time vector to a csv file
+    ofstream file;
+    file.open("time.csv");
+    file << "A,B" << endl;
+    for(int i = 0; i < time_vector.size(); i++){
+        file << time_vector[i][0] << "," << time_vector[i][1] << endl;
+    }
+    file.close();
+
 
     return 0;
 }
--- search.h
+++ search.h
@@ -1,5 +1,28 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-void read_text(string filename, int iNumThreads);
+// Struct to store the return values of the search function
+struct search_return{
+    int iNumThreads;
+    int threadSize;
+    int processTime;
+    int searchTime;
+    int totalTime;
+    int loveCount;
+    int hateCount;
+    string mostUsedWord;
+};
+
+// Function to count the number of occurrences of the words
+void count_words(int threadID,vector<vector<int>> count_vector ,string text, string word1, string word2);
+
+// Function to sum the number of occurrences of the words
+vector<int> somaThread(vector<vector<int>> count_vector, int iNumThreads);
+
+// Function to read the text from a file
+vector<string> read_text(string filename, int iNumThreads);
+
+// Function to process multiple threads
+search_return process_threads(int iNumThreads, string filename, string word1, string word2);
