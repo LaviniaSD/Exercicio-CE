1baf84240ffdfbb471aa6ad7e0c8c72c401a464d
IsabelaYabe <103371464+IsabelaYabe@users.noreply.github.com>
2024-03-08 14:58:28 -0300

    corrigir fc view

--- view_threads.py
+++ view_threads.py
@@ -7,12 +7,11 @@ def view_threads_all(csv_file):
 
     plt.scatter(df["A"], df["B"])
 
-    plt.xlabel("Número de Threads")
-<<<<<<< Updated upstream
+
     plt.ylabel("Tempo de Execução")
-=======
+
     plt.ylabel("Tempo de Execusão")
->>>>>>> Stashed changes
+
     plt.title("Análise de Paralelismo com Threads")
     plt.show()
 
@@ -29,20 +28,19 @@ def view_threads_searches(csv_file):
 def view_threads(csv_file):
     df = pd.read_csv(csv_file)
 
-    fig, axs = plt.subplots(1,2, figsize=(10,4))
+    fig, axs = plt.subplots(1, 2, figsize=(12, 6)) 
 
-    axs[0].plt.scatter(df["A"], df["B"])
+    axs[0].scatter(df["A"], df["B"]) 
 
-    axs[0].plt.xlabel("Número de Threads")
-    axs[0].plt.ylabel("Tempo de Execusão")
-    axs[0].plt.title("Análise de Paralelismo com Threads")
-    
-    axs[1].plt.scatter(df["A"], df["B"])
+    axs[0].set_xlabel("Número de Threads") 
+    axs[0].set_ylabel("Tempo de Execusão")  
+    axs[0].set_title("Análise de Paralelismo com Threads")  
 
-    axs[1].plt.xlabel("Número de Threads")
-    axs[1].plt.ylabel("Tempo de Execusão da Pesquisa")
-    axs[1].plt.title("Análise de Paralelismo com Threads")
-    axs[1].plt.show()
+    axs[1].scatter(df["A"], df["B"]) 
+
+    axs[1].set_xlabel("Número de Threads")  
+    axs[1].set_ylabel("Tempo de Execusão da Pesquisa")  
+    axs[1].set_title("Análise de Paralelismo com Threads")  
 
     plt.tight_layout()
     plt.show()
